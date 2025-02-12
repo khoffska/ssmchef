ce3c527c970ce3279b31824b928f7f2966f66f8b
khoffska <45382758+khoffska@users.noreply.github.com>
2025-02-11 23:07:48 -0500

    asfasf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "01terraformstatebucketzxczxc"       # Your S3 bucket name
+    key            = "/chefec2/state/terraform.tfstate"           # Path within the bucket to store the state file
+    region         = "us-east-1"                         # AWS region for the bucket
+    encrypt        = true                                # Enable server-side encryption for the state file
+    dynamodb_table = "terraform-locks-ec2chef"                   # DynamoDB table for state locking
+  }
+}
