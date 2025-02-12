247b29f7a147599c0947615ac5a9a68bba38a4dc
khoffska <45382758+khoffska@users.noreply.github.com>
2025-02-12 16:23:20 -0500

    asdf

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -7,18 +7,23 @@ resource "aws_ssm_association" "chef_association" {
   }
 
   parameters = {
-    SourceType             = "S3"
-    # Ensure the S3 URL is publicly accessible or the instance can access it via appropriate IAM roles.
-    SourceInfo             = "{\"path\":\"https://chefcookbooks9999.s3.amazonaws.com/docker-11.9.2.tar.gz\"}"
-    RunList                = "recipe[docker::default]"  # Adjust to your desired cookbook and recipe.
-    JsonAttributesSources  = "-"
-    JsonAttributesContent  = "-"
-    ChefClientVersion      = "14"
-    ChefClientArguments    = "-"
-    WhyRun                 = "False"
-    ComplianceSeverity     = "None"
-    ComplianceType         = "Custom:Chef"
-    ComplianceReportBucket = "-"
-    ChefExecutionTimeout   = "3600"
+    SourceType             = "GitHub"
+    # Ensure the S3 URL is publicly accessible or the instance can access it via appropriate IAM roles. 
+    SourceInfo = jsonencode({
+      owner      = "sous-chefs",
+      repository = "docker",
+      path       = "",            // Path within the repo, if needed (empty if at root)
+      getOptions = "branch:main"  // Adjust if you want to pull a different branch
+    })
+    RunList               = "recipe[docker]"  // Change to a wrapper recipe if needed
+    JsonAttributesSources = "-"
+    JsonAttributesContent = "-"
+    ChefClientVersion     = "14"
+    ChefClientArguments   = "-"
+    WhyRun                = "False"
+    ComplianceSeverity    = "None"
+    ComplianceType        = "Custom:Chef"
+    ComplianceReportBucket= "-"
+    ChefExecutionTimeout  = "3600"
   }
 }
