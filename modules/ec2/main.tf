748b42e5f8b82aa9eb6f30575159ea68da993c78
khoffska <45382758+khoffska@users.noreply.github.com>
2025-02-11 23:36:41 -0500

    asdf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,6 +2,10 @@ resource "aws_instance" "chef_instance" {
   ami                  = var.ami
   instance_type        = var.instance_type
   iam_instance_profile = var.iam_instance_profile
-
+  user_data = <<-EOF
+    #!/bin/bash
+    # Install Chef Client using the official Omnitruck installer
+    curl -L https://omnitruck.chef.io/install.sh | sudo bash
+  EOF
   tags = var.tags
 }
