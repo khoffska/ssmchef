5f68aaab8e39fa03b5685d29b61450671dae9fd6
khoffska <45382758+khoffska@users.noreply.github.com>
2025-02-11 23:08:47 -0500

    fasfasf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,13 +1,13 @@
 variable "role_name" {
   description = "IAM role name for EC2 instance"
   type        = string
-  default     = "ec2_ssm_role"
+  default     = "ec2_ssm_role2"
 }
 
 variable "instance_profile_name" {
   description = "Instance profile name for EC2 instance"
   type        = string
-  default     = "ec2_ssm_profile"
+  default     = "ec2_ssm_profile2"
 }
 
 variable "ssm_policy_arn" {
